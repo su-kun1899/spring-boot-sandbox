d234e6923ed74c7638405ae4b9e1c8ba1692d5d0
su-kun1899 <higedrum.coz@gmail.com>
2018-05-23 20:11:06 +0900

    first commit

---
+++ src/main/java/red/sukun1899/springbootsandbox/SpringBootSandboxApplication.java
@@ -0,0 +1,12 @@
+package red.sukun1899.springbootsandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSandboxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSandboxApplication.class, args);
+    }
+}
