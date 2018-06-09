716a64aeabb8feb1160ef059e41e449d9f667d52
su-kun1899 <higedrum.coz@gmail.com>
2018-06-09 22:09:26 +0900

    RoomServiceの作成

--- src/main/kotlin/red/sukun1899/springbootsandbox/domain/repository/ReservableRoomRepository.kt
+++ src/main/kotlin/red/sukun1899/springbootsandbox/domain/repository/ReservableRoomRepository.kt
@@ -1,6 +1,7 @@
 package red.sukun1899.springbootsandbox.domain.repository
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 import red.sukun1899.springbootsandbox.domain.model.ReservableRoom
 import red.sukun1899.springbootsandbox.domain.model.ReservableRoomId
 import java.time.LocalDate
@@ -8,6 +9,7 @@ import java.time.LocalDate
 /**
  * @author su-kun1899
  */
+@Repository
 interface ReservableRoomRepository : JpaRepository<ReservableRoom, ReservableRoomId> {
     fun findByReservableRoomeId_reservedDateOrderByReservableRoomId_roomIdAsc(reservedDate: LocalDate): List<ReservableRoom>
 }
---
+++ src/main/kotlin/red/sukun1899/springbootsandbox/domain/service/RoomService.kt
@@ -0,0 +1,18 @@
+package red.sukun1899.springbootsandbox.domain.service
+
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import red.sukun1899.springbootsandbox.domain.model.ReservableRoom
+import red.sukun1899.springbootsandbox.domain.repository.ReservableRoomRepository
+import java.time.LocalDate
+
+/**
+ * @author su-kun1899
+ */
+@Service
+@Transactional
+class RoomService(private val reservableRoomRepository: ReservableRoomRepository) {
+
+    fun findReservableRooms(date: LocalDate): List<ReservableRoom> =
+            reservableRoomRepository.findByReservableRoomeId_reservedDateOrderByReservableRoomId_roomIdAsc(date)
+}
