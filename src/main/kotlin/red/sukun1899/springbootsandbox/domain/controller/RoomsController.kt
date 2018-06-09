69c126608c229d706276ee642b0fbec7c205eb41
su-kun1899 <higedrum.coz@gmail.com>
2018-06-09 22:35:52 +0900

    特定の日付の予約会議室一覧を取得するハンドラを作成

--- src/main/kotlin/red/sukun1899/springbootsandbox/domain/controller/RoomsController.kt
+++ src/main/kotlin/red/sukun1899/springbootsandbox/domain/controller/RoomsController.kt
@@ -1,8 +1,10 @@
 package red.sukun1899.springbootsandbox.domain.controller
 
+import org.springframework.format.annotation.DateTimeFormat
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import red.sukun1899.springbootsandbox.domain.service.RoomService
 import java.time.LocalDate
@@ -18,6 +20,13 @@ class RoomsController(private val roomService: RoomService) {
         val today = LocalDate.now()
         val rooms = roomService.findReservableRooms(today)
         model.addAttribute("date", today)
+        model.addAttribute("rooms", rooms)
+        return "room/listRooms"
+    }
+
+    @GetMapping("{date}")
+    fun listRooms(@DateTimeFormat(iso = DateTimeFormat.ISO.DATE) @PathVariable("date") date: LocalDate, model: Model): String {
+        val rooms = roomService.findReservableRooms(date)
         model.addAttribute("rooms", rooms)
         return "room/listRooms"
     }
